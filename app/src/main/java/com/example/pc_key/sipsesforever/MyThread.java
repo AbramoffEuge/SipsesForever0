ca814fb51ed1e998b61ea77749b0300ec45f5546
AbramoffEuge <evgenabramov1@gmail.com>
2017-06-19 14:02:27 +0300

    Фон в потоке вывода на экран

--- app/src/main/java/com/example/pc_key/sipsesforever/MyThread.java
+++ app/src/main/java/com/example/pc_key/sipsesforever/MyThread.java
@@ -1,7 +1,12 @@
 package com.example.pc_key.sipsesforever;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.view.SurfaceHolder;
 
 /**
@@ -9,11 +14,17 @@ import android.view.SurfaceHolder;
  */
 
 public class MyThread extends Thread {
+    private Paint paint = new Paint();
     private SurfaceHolder surfaceHolder;
     Context context;
     private volatile boolean running = false;
     private static float deltaT = 0;
     private int w, h;
+    //private RectF dstBoard = new RectF();
+    //private Bitmap board;
+    public float x, y;
+    private Bitmap backGr;
+    private RectF dstBackGr;
 
     public static float getDeltaT() {
         return deltaT;
@@ -27,6 +38,8 @@ public class MyThread extends Thread {
         this.w = w;
         this.h = h;
         this.surfaceHolder = surfaceHolder;
+        backGr = BitmapFactory.decodeResource(context.getResources(), R.mipmap.my_backgr);
+        dstBackGr = new RectF(0, 0, w, h);
     }
 
     public void setRunning(boolean running) {
@@ -38,16 +51,18 @@ public class MyThread extends Thread {
         Canvas canvas = null;
         double lastTime = System.currentTimeMillis() / 1000.0;
         double currentTime;
+        paint.setColor(Color.CYAN);
 
         while (running){
             canvas = surfaceHolder.lockCanvas();
             if (canvas != null)
                 try {
                     synchronized(surfaceHolder){
+                        canvas.drawBitmap(backGr, null, dstBackGr, paint);
                         currentTime = System.currentTimeMillis() / 1000.0;
                         deltaT += (float) (currentTime - lastTime);
                         lastTime = currentTime;
-                        canvas.drawRect(0, 0, w, h, null);
+                        //canvas.drawRect(0, 0, w, h, paint);
                         updateAll();
                         drawAll(canvas);
                     }
@@ -58,6 +73,14 @@ public class MyThread extends Thread {
                 }
         }
     }
+
+    /*void checkTouchDown(float xx, float yy){
+        if ((yy > h - board.getHeight() - 75)
+                & (xx < dstBoard.centerX() + board.getWidth()/ 2 + 50)
+                & (xx > dstBoard.centerX() - board.getWidth()/ 2 - 50)) {
+            x = xx;
+        }
+    }*/
 
     private void updateAll(){
 
