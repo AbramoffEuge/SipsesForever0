92406a920866354d44ec542ddbaadbfd0981bfa9
AbramoffEuge <evgenabramov1@gmail.com>
2017-06-19 12:18:01 +0300

    Создал классы шарика, доски, блока

---
+++ app/src/main/java/com/example/pc_key/sipsesforever/Board.java
@@ -0,0 +1,29 @@
+package com.example.pc_key.sipsesforever;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+
+/**
+ * Created by PC_key on 19.06.2017.
+ */
+
+public class Board {
+    Paint paint = new Paint();
+    float x, y;
+    Bitmap board;
+    RectF dstBoard = new RectF();
+
+    public Board(float x, float y, Bitmap board) {
+        this.x = x;
+        this.y = y;
+        this.board = board;
+    }
+
+    public void draw(Canvas canvas) {
+        dstBoard.set(x - board.getWidth() / 2, y - board.getHeight() / 2,
+                x + board.getWidth() / 2, y + board.getHeight() / 2);
+        canvas.drawBitmap(board, null, dstBoard, paint);
+    }
+}
