3ff12e4a3995ba7dda7a41d2e25f24958c2c032c
MassakaKodes <utuning0302@gmail.com>
2017-06-20 13:05:19 +0300

    скрыта верхняя строка

--- app/src/main/java/com/example/pc_key/sipsesforever/Main3Activity.java
+++ app/src/main/java/com/example/pc_key/sipsesforever/Main3Activity.java
@@ -3,14 +3,18 @@ package com.example.pc_key.sipsesforever;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Main3Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//скрытие верхей сторки
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//скрытие верхей сторки
+
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main3);
     }
