3b65470381348f9a6ee3b00a6e021855435b0131
AbramoffEuge <evgenabramov1@gmail.com>
2017-06-22 01:03:08 +0300

    Сделал скелет меню

--- app/src/main/java/com/example/pc_key/sipsesforever/Main3Activity.java
+++ app/src/main/java/com/example/pc_key/sipsesforever/Main3Activity.java
@@ -1,8 +1,10 @@
 package com.example.pc_key.sipsesforever;
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -15,5 +17,10 @@ public class Main3Activity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//скрытие верхей сторки
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main3);
+    }
+    public void onMyButtonClick3(View v) {
+        Intent intent = new Intent(Main3Activity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 }
